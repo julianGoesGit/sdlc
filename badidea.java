3d0ce8203dc3b8f6ea753344e84e569816a3fb66
julianGoesGit <63906903+julianGoesGit@users.noreply.github.com>
2022-03-03 22:00:00 +0100

    Add files via upload

---
+++ badidea.java
@@ -0,0 +1,9 @@
+package com.securitywhitepapers.hardcodedpassword;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+        String password = "somehiddenpassword";
+    }
+
+}
